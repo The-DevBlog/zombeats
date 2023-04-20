02255392eab519f48ed8472b256308e751013fca
andrew <andrewcs149@gmail.com>
2023-04-19 22:14:33 -0700

    comment out systems for debugging

--- src/game/camera/mod.rs
+++ src/game/camera/mod.rs
@@ -12,7 +12,13 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
-            (orbit_gamepad, orbit_mouse, zoom_gamepad, zoom_mouse).in_set(OnUpdate(AppState::Game)),
+            (
+                // orbit_gamepad,
+                orbit_mouse,
+                zoom_gamepad,
+                zoom_mouse,
+            )
+                .in_set(OnUpdate(AppState::Game)),
         );
     }
 }
